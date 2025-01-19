7c3c9ec07db174e3cfd79e453c1e5287f80d0c71
Pankaj Maurya <100466858+Pankaj352@users.noreply.github.com>
2025-01-19 14:14:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,25 @@
 //your code here!
 
+let totalList=10;
+let ol=document.getElementById('infi-list');
+
+window.addEventListener('load',()=>{
+	for (let index = 1; index <= totalList; index++) {
+		let li=document.createElement('li');
+		li.innerText=`Item ${index}`;
+		ol.append(li);
+	}
+
+	ol.addEventListener('scroll',()=>{
+		let height=ol.offsetHeight;
+		let offset=ol.scrollTop;
+
+		if(offset+height>=ol.scrollHeight){
+			for(let i=1;i<=2;i++){
+				let li=document.createElement('li');
+			li.innerText=`Item ${++totalList}`;
+			ol.append(li);
+			}
+		}
+	})
+})
